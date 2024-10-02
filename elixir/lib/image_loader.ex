7127d665260633a6ffb8a1b26be9c4b01edc0bcd
Ethan Carollo <etcarollo@gmail.com>
2024-10-02 16:14:58 +0200

    🔧 fix image loading

--- elixir/lib/image_loader.ex
+++ elixir/lib/image_loader.ex
@@ -83,22 +83,17 @@ defmodule ImageLoader do
   end
 
   def train_test_split(images, labels, test_size \\ 0.2) do
-    # Get the total number of images
     num_images = elem(Nx.shape(images), 0)  # Get the size of the first dimension (number of images)
 
-    # Calculate the split index for training set size
-    IO.inspect(num_images)
     split_index = round(num_images * (1.0 - test_size))
 
-    # Ensure split_index is within bounds
     split_index = min(split_index, num_images)
 
-    # Split images and labels into train and test sets
-    # TODO : here there is a problem, will fix later cause 😴
-    {X_train, X_test} = Nx.split(images, split_index)
-    {y_train, y_test} = Nx.split(labels, split_index)
+    {x_train, x_test} = Nx.split(images, split_index, axis: 0)
+    {y_train, y_test} = Nx.split(labels, split_index, axis: 0)
 
-    {X_train, X_test, y_train, y_test}
+
+    {x_train, x_test, y_train, y_test}
   end
 
 end
--- elixir/lib/pika_cnn.ex
+++ elixir/lib/pika_cnn.ex
@@ -1,16 +1,16 @@
 defmodule PikaCNN do
   def run do
-    directory_path = "../data/pokemon"
-    max_images_per_label = 1000
+    directory_path = "../data/pokemon-128augmented"
+    max_images_per_label = 20
 
     # This should be good
     {image_paths, labels, label_names} = ImageLoader.load_images_from_directory(directory_path, max_images_per_label)
 
     {images, labels} = ImageLoader.images_to_tensor(image_paths, labels, label_names)
-    {X_train, X_test, y_train, y_test} = ImageLoader.train_test_split(images, labels, 0.2)
+    {x_train, x_test, y_train, y_test} = ImageLoader.train_test_split(images, labels, 0.2)
 
-    IO.inspect("Training images: #{Nx.shape(X_train)}")
-    IO.inspect("Testing images: #{Nx.shape(X_test)}")
+    IO.inspect("Training images: #{elem(Nx.shape(x_train), 0)}")
+    IO.inspect("Testing images: #{elem(Nx.shape(x_test),0)}")
     IO.inspect("Number of classes: #{length(label_names)}")
     IO.inspect("Class names: #{label_names}")
   end
